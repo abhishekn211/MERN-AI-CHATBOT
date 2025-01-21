12f564e862de2d37983fda454703df5aac269d23
abhishekn211 <150190422+abhishekn211@users.noreply.github.com>
2025-01-21 21:21:43 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5174", credentials: true }));
+app.use(cors({ origin: "https://mern-ai-chatbot-frontend-n7lp.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
