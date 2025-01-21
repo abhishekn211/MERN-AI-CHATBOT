f568f7ef28cd6d210c8f3ff8042961c680288ea5
abhishekn211 <150190422+abhishekn211@users.noreply.github.com>
2025-01-21 21:52:43 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,6 +16,6 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 //remove it in production
 //app.use(morgan("dev"));
 
-app.use("/api/v1", appRouter);
+app.use("https://mern-ai-chatbot-frontend-n7lp.onrender.com", appRouter);
 
 export default app;
